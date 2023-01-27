95d86d3c18be421148166ba78a4585112aecdbbe
usuario <usuario@LENOVO>
2023-01-27 12:57:52 +0100

    2ª modificacion

--- Proyecto1Git/src/paquete1/principal.java
+++ Proyecto1Git/src/paquete1/principal.java
@@ -4,6 +4,7 @@ public class principal {
 
 	public static void main(String[] args) {
 		System.out.println("Estamos haciendo control de versiones");
+		System.out.println("una modificacion");
 	}
 
 }
