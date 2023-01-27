35fd40915c3e8efe2d79f643c2ecd6df2a244878
usuario <usuario@LENOVO>
2023-01-27 13:18:14 +0100

    una tercera modificacion

--- Proyecto1Git/src/paquete1/principal.java
+++ Proyecto1Git/src/paquete1/principal.java
@@ -5,6 +5,7 @@ public class principal {
 	public static void main(String[] args) {
 		System.out.println("Estamos haciendo control de versiones");
 		System.out.println("una modificacion correcta");
+		System.out.println("una 3ª modificacion correcta");
 	}
 
 }
